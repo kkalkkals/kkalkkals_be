a1ef49cce44e321dd2b9e80a79de639732964fe6
sein <si2238@naver.com>
2025-03-05 23:24:37 +0900

    feat: add request creation api
    대행요청 글 작성 기능 api 구현

---
+++ controllers/postController.js
@@ -0,0 +1,29 @@
+import { insertPost } from "../models/postModel.js";
+
+export async function addPost(req, res) {
+  const { trash_type, trash_amount, address, request_term, money } = req.body;
+  const image = req.file ? `${req.file.key}` : null;
+
+  if (!trash_type || !trash_amount || !address || !money || !image) {
+    return res.status(400).json({ message: "필수안보냄" });
+  }
+
+  try {
+    const postId = await insertPost({
+      trash_type,
+      trash_amount,
+      address,
+      request_term,
+      image,
+      money,
+    });
+
+    return res.status(201).json({
+      message: "새 건 추가 완",
+      postId: postId,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "서버 오류 발생" });
+  }
+}
