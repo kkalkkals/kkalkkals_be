1d4d1f72da2a5577571e8dc86669c77ec58355e3
jenny.kang <deco1202@naver.com>
2025-03-05 23:45:26 +0900

    feat: add get request api

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { testDB } from "./controllers/test.js";
+// import { testDB } from "./controllers/test.js";
+import postsRouter from './routes/pickupRoutes.js';
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
+app.use('/api/posts', postsRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-testDB();
+// testDB();
